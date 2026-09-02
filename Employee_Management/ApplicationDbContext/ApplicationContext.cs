409c5809a590fad76581ee70be51fc6b52aaf0ed
Prithvi1713 <plavate95@gmail.com>
2026-09-02 22:51:55 +0530

    Add project files.

---
+++ Employee_Management/ApplicationDbContext/ApplicationContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management.ApplicationDbContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
