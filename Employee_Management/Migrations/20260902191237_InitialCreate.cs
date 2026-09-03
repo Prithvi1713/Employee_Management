0e2642bd11f8332580e24a376ba7676ded073548
Prithvi1713 <plavate95@gmail.com>
2026-09-03 08:54:52 +0530

    DepartmentMaster Controller Created and perform Migration successfully.

---
+++ Employee_Management/Migrations/20260902191237_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employee_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "departmentMasters",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentCode = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    DepartmentName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departmentMasters", x => x.DepartmentId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "departmentMasters");
+        }
+    }
+}
